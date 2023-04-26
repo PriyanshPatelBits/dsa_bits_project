9e888e9da8fd1faa41e39907f9addeca92308a77
PriyanshPatelBits <102814579+PriyanshPatelBits@users.noreply.github.com>
2023-04-26 23:02:05 +0530

    priyansh added val, randLeaf and insert function

---
+++ pp.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#define T INT_MAX
+#define MOULD_SIZE 50
+
+typedef struct lnode * Lnode;
+typedef struct mnode * Mnode;
+
+struct lnode{
+    int value;
+    Lnode next;
+};
+struct mnode {
+    Lnode list;
+    bool dirty;
+    // i have omitted integer c as it was unnecessary for this project
+};
+
+Lnode createLnode(int value){
+    Lnode ln = (Lnode) malloc(sizeof(struct lnode));
+    ln->value = value;
+    ln->next = NULL;
+    return ln;
+}
+int randLeaf(int d){
+    srand(time(NULL));
+    int ran, low = (int) pow(2,d-1), upp = (int)(pow(2,d) + 1);
+    ran = (rand() % (upp - low + 1)) + low;
+    return ran;
+}
+
+int val(Mnode mn){
+    if (mn->list == NULL) return T;
+    return mn->list->value;
+}
+
+int binarySearchLeaf();
+
+int findInsertPoint(Mnode arr[], int value);
+
+void mouldify(Mnode arr[], Mnode nod);
+
+int insert(Mnode arr[], int value){
+    Lnode ln = createLnode(value);
+    Mnode ip = arr[findInsertPoint(arr, value)];
+    ln->next = ip->list;
+    ip->list = ln;
+    ip->dirty = true;
+    mouldify(arr, ip);
+}
+
+int main(){
+    Mnode tree[MOULD_SIZE];
+    int depth = 1;
+    for(int i=0;i<MOULD_SIZE;i++){
+        tree[i] = (Mnode) malloc(sizeof(struct mnode));
+        tree[i]->list = NULL;
+        tree[i]->dirty = false;
+    }
+    return 0;
+}
